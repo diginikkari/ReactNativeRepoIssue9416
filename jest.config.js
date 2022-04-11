e8ff63109e1f5fca835cff341ec26d04a36f4e84
Timo Santi <timo.santi@santicon.fi>
2022-04-11 11:25:24 +0300

    tests working

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  //   transform: {
-  //     '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
-  //     '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
-  //       'react-native/jest/assetFileTransformer.js',
-  //     ),
-  //   },
   transformIgnorePatterns: [
     'node_modules/(?!(@react-native|react-native)/|react-native)',
   ],
